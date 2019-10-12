d06475a591eab7c86016739543fb7c8c7391a43b
casallas123 <andresf-casallasa@unilibre.edu.co>
2019-10-12 12:05:34 -0500

    primer comit

---
+++ src/main/java/com/example/demo/EjercicioMicroServicios1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMicroServicios1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioMicroServicios1Application.class, args);
+	}
+
+}
