b78321f01a2d4115a64bfb0686c9154e6976f91a
casallas123 <andresf-casallasa@unilibre.edu.co>
2019-10-12 13:10:42 -0500

    Proceso5 Creacion completa clase feign

---
+++ src/main/java/co/edu/unilibre/pedido/feign/InventarioServiceClientFeign.java
@@ -0,0 +1,19 @@
+package co.edu.unilibre.pedido.feign;
+
+import java.util.List;
+
+import org.springframework.ccloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import co.edu.unilibre.pedido.model.Inventario;
+
+@FeignClient (name = "product-service", url = "localhost:10010")
+public interface InventarioServiceClientFeign {
+	
+	@GetMapping("/list")
+	public List<Inventario> getList();
+	
+	@GetMapping("/{id}")
+	public Inventario getById(@PathVariable Long id);
+}
